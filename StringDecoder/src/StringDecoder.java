a5e33354389b3d612881d2d302f36eff67129697
A00239007 <A00239007@Z308-PC17.studentnet.ait.ie>
2017-09-19 13:03:43 +0100

    The product code for the string decoder has been finsihed

--- StringDecoder/src/StringDecoder.java
+++ StringDecoder/src/StringDecoder.java
@@ -1,28 +1,70 @@
 import java.util.Scanner;
 public class StringDecoder {
 	//main method(Call for other methods & validation for number)
-	public static void main(String [] args){
-		
-	}
-	//Read my string method
-	public String readMyString(){
-		
-	}
-	//Length method
-	public int getLength(String str){
+	public static void main(String [] args) throws Exception{
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a word");
 		String word = in.next();
-		int length = word.length();
-		return length;
+		if(word.endsWith(String.valueOf(word.charAt(0))))
+		{
+			System.out.println("The word has "+getLength(word)+" letters.");
+			System.out.println("The word has "+numOfVowels(word)+" vowels.");
+			System.out.println(getLetters(word));
+		}
+		else
+		{
+			throw new Exception("The user entered a number");
+		}
+		
+	}
+	//Length method
+	public static int getLength(String str)throws Exception{
+		if(str.endsWith(String.valueOf(str.charAt(0))))
+		{
+			int length = str.length();
+			return length;
+		}
+		else
+		{
+			throw new Exception("The user entered a number");
+		}
+		
 	}
 	//Vowels method
-	public int numOfVowels(String str){
-		
+	public static int numOfVowels(String str)throws Exception{
+		if(str.endsWith(String.valueOf(str.charAt(0))))
+		{
+			str = str.toLowerCase();
+			int vowels = 0;
+			int i = 0;
+			for(i = 0; i<= str.length(); i++)
+			{
+				char ch = str.charAt(i);
+				if(ch == 'a' || ch == 'i' || ch == 'e' || ch == 'o' || ch == 'u')
+				{
+					vowels++;
+				}
+			}
+			return vowels;
+		}
+		else
+		{
+			throw new Exception("The user entered a number");
+		}
 	}
 	//First letter and last letter method
-	public String firstAndLastLetter(String str){
-		
+	public static String getLetters(String str)throws Exception{
+		if(str.endsWith(String.valueOf(str.charAt(0))))
+		{
+			char firstLetter = str.charAt(0);
+			char lastLetter = str.charAt(str.length() - 1);
+			String results = firstLetter+" , "+lastLetter;
+			return results;
+		}
+		else
+		{
+			throw new Exception("The user entered a number");
+		}
 	}
 
 }
--- StringDecoder/src/StringDecoderTest.java
+++ StringDecoder/src/StringDecoderTest.java
@@ -5,7 +5,10 @@ public class StringDecoderTest extends TestCase {
 	// Test: 001
 	// Objective: Test if the method that the method takes strings
 	// Input(s): Apple
-	// Output(s): No outputs are expected
+	// Output(s): The user entered a number
+	public void test001(){
+	
+	}
 	
 	//Test: 002
 	//Objective: Test if the method gets the length of the string
