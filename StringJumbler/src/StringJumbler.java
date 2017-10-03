a5f28fe1060dbae81c942a0b5035f2c6be3f72c5
A00239007 <A00239007@Z308-PC17.studentnet.ait.ie>
2017-10-03 12:53:29 +0100

    Product code has been edited

--- StringJumbler/src/StringJumbler.java
+++ StringJumbler/src/StringJumbler.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StringJumbler {
@@ -9,19 +10,20 @@ public class StringJumbler {
 		StringJumbler g = new StringJumbler();
 		String file = g.analyseString();
 		System.out.println("They're "+g.countWords(file)+" words in this file.");
+		System.out.println("They're "+g.countVowels(file)+" words in this file.");
 	}
 	
 	public String analyseString() throws FileNotFoundException{
 		File f = new File("C:\\Users\\A00239007\\Desktop\\text.txt");
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(f);
-		String s = sc.nextLine();
+		String s = sc.next();
 		return s;
 	}
 	
 	public int countWords(String s){
 		int count = 0;
-		for(int i = 0; i<s.length(); i++)
+		for(int i = 0; i < s.length(); i++)
 		{
 			char current = s.charAt(i);
 			if(current == ' ')
@@ -34,15 +36,26 @@ public class StringJumbler {
 	
 	public int countVowels(String s){
 		int count = 0;
-		for(int i = 0; i<s.length(); i++)
+		for(int i = 0; i < s.length(); i++)
 		{
 			char current = s.charAt(i);
-			if(current == {'e','a','i','o'})
+			if(current == 'e' || current == 'i' || current == 'a' || current == 'o' || current == 'u')
 			{
 				count++;
 			}
 		}
 		return count;
+	}
+	
+	public String jumbleString(String s)
+	{
+		
+		ArrayList<String> t = new ArrayList<>();
+		for(int i = 0; i < s.length(); i++)
+		{
+			
+		}
+		return s;
 	}
 	
 }
