876fe9a6561339424a4b95be7d0e0bb44e20b26e
A00239007 <A00239007@Z104-PC12.studentnet.ait.ie>
2017-11-07 11:57:35 +0000

    Porduct Code satted

---
+++ Lab6/src/Books.java
@@ -0,0 +1,19 @@
+import java.awt.print.Book;
+
+
+public  class Books implements Comparable<Book>{
+	private static int sortMethod = 1;
+	
+	private String title,author,publishitar;
+	private double bullyets;
+	
+	public Book(String title,String author,String publishitar,double bullyets)
+	{
+		
+	}
+	@Override
+	public int compareTo(Book o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
