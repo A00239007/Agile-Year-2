25864e0550badcc8623eca13cd7168e772ade102
Simon Harper <A00239007@student.ait.ie>
2018-02-26 09:58:11 +0000

    Work done on Lab10
    Working on how the arraylist prints string arrays on the console

--- Lab10/src/LiveRegisterDirectory.java
+++ Lab10/src/LiveRegisterDirectory.java
@@ -10,16 +10,22 @@ public class LiveRegisterDirectory {
 	public static void main(String [] args) throws FileNotFoundException
 	{
 		File csvFile = new File("C:\\Users\\A00239007\\Downloads\\all_persons_under_25.csv");
-		String line = "";
+		String[] line = new String[130];
+		int count = 0;
 		Scanner sc = new Scanner(csvFile);
 		ArrayList<String[]> list = new ArrayList<String[]>();
 		while(sc.hasNextLine())
 		{
-			line = sc.nextLine();
-			String[] row = line.split(",");
-			list.add(row);
+			line[count] = sc.nextLine();
+			list.add(line);
+			count++;
 		}
-		System.out.println(list);
-		
+		for(String[] print : list)
+		{
+			for(int i = 0; i<print.length; i++)
+			{
+				System.out.println(print[i]);
+			}
+		}
 	}
 }
