f74197d238dfdc2478d319a899136521e4aac3d0
Simon Harper <A00239007@student.ait.ie>
2018-03-07 09:59:53 +0000

    Finished Example 1 on SAX parsing

---
+++ SAXParser/src/MyFirstSAXParser.java
@@ -0,0 +1,23 @@
+import java.io.ByteArrayInputStream;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+
+public class MyFirstSAXParser {
+	public static void main(String [] args)throws SAXException, Exception
+	{
+		String xml = "<?xml version='1.0'?><class><student></student></class>";
+		ByteArrayInputStream input = new ByteArrayInputStream(xml.getBytes("UTF-8"));
+		
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser saxParser = factory.newSAXParser();
+		SAXParserEventHandler eventHandler = new SAXParserEventHandler();
+		saxParser.parse(input, eventHandler);;
+	}
+}
---
+++ SAXParser/src/SAXParserEventHandler.java
@@ -0,0 +1,17 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+public class SAXParserEventHandler extends DefaultHandler {
+	
+	public void startElement(String uri, String localName, String qName, Attributes attributes)throws SAXException
+	{
+		System.out.println("Start Element: "+qName);
+	}
+	
+	public void endElement(String uri, String localName, String qName)throws SAXException
+	{
+		System.out.println("End Element: "+qName);
+	}
+}
